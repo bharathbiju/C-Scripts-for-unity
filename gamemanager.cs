0d9cf68068a9e6df11d21bf3c1e8db19e664e85f
Bharath Biju <bharathinfoclg@gmail.com>
2020-07-01 18:20:03 +0530

    Add files via upload

---
+++ gamemanager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gamemanager : MonoBehaviour
+{
+    
+    public GameObject completedlevelt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void completedlevel()
+    {
+        
+        completedlevelt.SetActive(true);
+    }
+    
+    public void End(Vector3 y ,Vector3 z, GameObject x)
+    {
+
+
+//x.transform.position = y;
+       // x.transform.eulerAngles =z;
+        
+        
+    }
+    void restart()
+    {
+        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ splitcam.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class splitcam : MonoBehaviour
+{
+    public Camera cam1, cam2;
+    private bool isHorizontal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam1.rect = new Rect(0f, .5f, 1f, .5f);
+        cam2.rect = new Rect(0f, 0f, 1f, .5f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            isHorizontal = !isHorizontal;
+            SetSplitScreen();
+        }
+        
+    }
+
+    public void SetSplitScreen()
+    {
+        if(isHorizontal)
+        {
+            cam1.rect = new Rect(0f, .5f, 1f, .5f);
+            cam2.rect = new Rect(0f, 0f, 1f, .5f);
+
+        }
+        else
+        {
+            cam1.rect = new Rect(0f,0f,.5f,1f);
+            cam2.rect = new Rect(.5f, 0f, .5f, 1f);
+        }
+    }
+}
